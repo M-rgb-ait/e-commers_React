c8d49b4366278489d4a0bd66a13c8837e230ed2e
عطا عطيه <M-rgb-ait>
2025-07-31 13:04:38 +0300

    updata wishlist

--- src/Componante/HomeSelider/HomeSelider.jsx
+++ src/Componante/HomeSelider/HomeSelider.jsx
@@ -1,10 +1,10 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import img1 from '../../assets/imgs/slider-image-1.jpeg';
-import img2 from '../../assets/imgs/slider-image-2.jpeg';
-import img3 from '../../assets/imgs/slider-image-3.jpeg';
-import img4 from '../../assets/imgs/slider-2.jpeg';
+import img1 from "../../assets/imgs/slider-image-1.jpeg";
+import img2 from "../../assets/imgs/slider-image-2.jpeg";
+import img3 from "../../assets/imgs/slider-image-3.jpeg";
+import img4 from "../../assets/imgs/slider-2.jpeg";
 
 export default function SimpleSlider() {
   var settings = {
@@ -16,29 +16,26 @@ export default function SimpleSlider() {
   };
   return (
     <div className="flex">
-
-
-        <div className="w-3/4">
-    <Slider {...settings} autoplay>
-      <div>
-        <img className=" w-full h-72" src={img1} alt="" />
+      <div className="w-3/4">
+        <Slider {...settings} autoplay>
+          <div>
+            <img className=" w-full h-72" src={img1} alt="" />
+          </div>
+          <div>
+            <img className=" w-full h-72" src={img2} alt="" />
+          </div>
+          <div>
+            <img className=" w-full h-72" src={img3} alt="" />
+          </div>
+          <div>
+            <img className=" w-full h-72" src={img4} alt="" />
+          </div>
+        </Slider>
       </div>
-      <div>
-      <img className=" w-full h-72" src={img2} alt="" />
-      </div>
-      <div>
-      <img className=" w-full h-72" src={img3} alt="" />
-      </div>
-      <div>
-      <img className=" w-full h-72" src={img4} alt="" />
-      </div>
-    </Slider>
-        </div>
-        <div className="w-1/4">
+      <div className="w-1/4">
         <img src={img1} className="w-full h-36 block" alt="" />
         <img src={img2} className="w-full h-36 block" alt="" />
-        </div>
-
+      </div>
     </div>
   );
 }
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -5,68 +5,63 @@ import { useState } from "react";
 
 export const cartContext = createContext();
 
+export default function CartContextProvider({ children }) {
+  const { useToken } = useContext(authcontext);
+  //HINT: to be sald=> multiple states (scersh)
+  // const [numOfCartItems, setnumOfCartItems] = useState(0);
+  const [totalCartPrice, settotalCartPrice] = useState(0);
+  const [products, setproducts] = useState(null);
+  const [cartId, setCartid] = useState(null);
 
-export default function CartContextProvider({children}) {
-const {useToken} = useContext(authcontext);
-//HINT: to be sald=> multiple states (scersh)
-// const [numOfCartItems, setnumOfCartItems] = useState(0);
-const [totalCartPrice, settotalCartPrice] = useState(0);
-const [products, setproducts] = useState(null);
-const [cartId, setCartid] = useState(null);
+  // console.log('cartId', cartId);
 
-// console.log('cartId', cartId);
+  //Dervied states الاشتقاق
+  const numOfCartItems = products?.length;
+  // const headers ={
+  //     useToken
+  // }
 
+  function restevalus() {
+    settotalCartPrice(0);
+    setproducts(null);
+    setCartid(null);
+  }
 
-
-
-
-
-//Dervied states الاشتقاق
-const numOfCartItems = products?.length;
-// const headers ={
-//     useToken
-// }
-
-function restevalus() {
-settotalCartPrice(0)
-setproducts(null)
-setCartid(null)
-}
-
-async function addprodectcart(id) {
-    const res = await axios.post('https://ecommerce.routemisr.com/api/v1/cart',
+  async function addprodectcart(id) {
+    const res = await axios
+      .post(
+        "https://ecommerce.routemisr.com/api/v1/cart",
         {
-            productId: id,
+          productId: id,
         },
         {
-            headers: {token: useToken}
-        })
-        .then(function (res){
-                //display maseges==this               this is asncrons
-                // setnumOfCartItems(res.data.numOfCartItems);
-                // settotalCartPrice(res.data.totalCartPrice);
-                // setproducts(res.data.products);
-                setCartid(res.data.cartId);
-                getUserCart();
-                return true;
-        })
-        .catch(function (err){
-            console.log('err',err);
-            return false;
-        })
-        return res;
-}
-
-
-
-
-function getUserCart() {
-    axios.get('https://ecommerce.routemisr.com/api/v1/cart',{
-        headers: {
-            token:useToken,
+          headers: { token: useToken },
         }
-    })
-    .then(function(resp) {
+      )
+      .then(function (res) {
+        //display maseges==this               this is asncrons
+        // setnumOfCartItems(res.data.numOfCartItems);
+        // settotalCartPrice(res.data.totalCartPrice);
+        // setproducts(res.data.products);
+        setCartid(res.data.cartId);
+        getUserCart();
+        return true;
+      })
+      .catch(function (err) {
+        console.log("err", err);
+        return false;
+      });
+    return res;
+  }
+
+  function getUserCart() {
+    axios
+      .get("https://ecommerce.routemisr.com/api/v1/cart", {
+        headers: {
+          token: useToken,
+        },
+      })
+      .then(function (resp) {
         // console.log('resp',resp.data.numOfCartItems);
         // console.log('resp',resp.data.data.totalCartPrice);
         // console.log('resp',resp.data.data.products);
@@ -74,111 +69,103 @@ function getUserCart() {
         settotalCartPrice(resp.data.data.totalCartPrice);
         setproducts(resp.data.data.products);
         setCartid(resp.data.cartId);
+      })
+      .catch(function (err) {
+        console.log("err", err);
+      });
+  }
 
-        
-        
-
-    })
-    .catch(function(err) {
-        console.log('err',err);
-        
-    })
-}
-
-async function UpdataCount(id, newCount) {
-    const res = await axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${id}`,
+  async function UpdataCount(id, newCount) {
+    const res = await axios
+      .put(
+        `https://ecommerce.routemisr.com/api/v1/cart/${id}`,
         {
-            "count": newCount
+          count: newCount,
         },
         {
-            headers: {
-                token: useToken,
-            }
+          headers: {
+            token: useToken,
+          },
         }
-    )
-    .then(function (resp) {
+      )
+      .then(function (resp) {
         // setnumOfCartItems(resp.data.numOfCartItems);
         settotalCartPrice(resp.data.data.totalCartPrice);
         setproducts(resp.data.data.products);
         return true;
-    })
-    .catch(function (err) {
-        console.log('err', err);
+      })
+      .catch(function (err) {
+        console.log("err", err);
         return false;
-    })
+      });
     return res;
-}
+  }
 
-
-async function RemovElementFromCart(id) {
-    const res = await axios.delete(`https://ecommerce.routemisr.com/api/v1/cart/${id}`,
-        {
-            headers: {
-                token: useToken,
-            }
-        }
-    )
-    .then(function (resp) {
+  async function RemovElementFromCart(id) {
+    const res = await axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/cart/${id}`, {
+        headers: {
+          token: useToken,
+        },
+      })
+      .then(function (resp) {
         // setnumOfCartItems(resp.data.numOfCartItems);
         settotalCartPrice(resp.data.data.totalCartPrice);
         setproducts(resp.data.data.products);
         return true;
-    })
-    .catch(function (err) {
-        console.log('err', err);
+      })
+      .catch(function (err) {
+        console.log("err", err);
         return false;
-    })
+      });
     return res;
-}
-async function RemoveAllFromCart() {
-    const res = await axios.delete(`https://ecommerce.routemisr.com/api/v1/cart`,
-        {
-            headers: {
-                token: useToken,
-            }
-        }
-    )
-    .then(function () {
+  }
+  async function RemoveAllFromCart() {
+    const res = await axios
+      .delete(`https://ecommerce.routemisr.com/api/v1/cart`, {
+        headers: {
+          token: useToken,
+        },
+      })
+      .then(function () {
         settotalCartPrice(0);
         setproducts(null);
         return true;
-    })
-    .catch(function (err) {
-        console.log('err', err);
+      })
+      .catch(function (err) {
+        console.log("err", err);
         return false;
-    })
+      });
     return res;
-}
+  }
 
-//componant did mount
-useEffect(() => {
+  //componant did mount
+  useEffect(() => {
     //on evrey refresh and for the first time
     if (useToken) {
-        getUserCart();
+      getUserCart();
     }
     // Login =>
-}, [useToken]);
-
-
-
-
+  }, [useToken]);
 
   return (
     <>
-    <cartContext.Provider value={{
-        addprodectcart,
-        getUserCart,
-        numOfCartItems,
-        totalCartPrice,
-        products,
-        UpdataCount,
-        RemovElementFromCart,
-        RemoveAllFromCart,
-        cartId,
-        restevalus,
-        }}>
+      <cartContext.Provider
+        value={{
+          addprodectcart,
+          getUserCart,
+          numOfCartItems,
+          totalCartPrice,
+          products,
+          UpdataCount,
+          RemovElementFromCart,
+          RemoveAllFromCart,
+          cartId,
+          restevalus,
+        }}
+      >
         {children}
-    </cartContext.Provider>
+      </cartContext.Provider>
     </>
-  )
+  );
 }
